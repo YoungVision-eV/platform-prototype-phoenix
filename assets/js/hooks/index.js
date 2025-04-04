ac93491c089d68320374d749697c6dc2841b3453
Jonas Bulik <jonas@bulik.dev>
2025-04-04 18:47:47 +0200

    Fix community map

--- assets/js/hooks/index.js
+++ assets/js/hooks/index.js
@@ -1,9 +1,11 @@
 import ScrollToBottom from "./scroll_to_bottom";
 import LoadUserList from "./load_user_list";
 import UserMenu from "./user_menu";
+import CommunityMap from "./community_map";
 
 export default {
   ScrollToBottom,
   LoadUserList,
-  UserMenu
+  UserMenu,
+  CommunityMap
 };
